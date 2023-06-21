036a4d040543cb0831cd4f003051e56200695ac6
muranox <naokimurano@outlook.com>
2023-06-21 16:59:48 +0900

    fix: bottom for social links mobile css

--- src/Page/Header.js
+++ src/Page/Header.js
@@ -12,10 +12,10 @@ export function Header() {
                 <img src={DoubleLogo} alt="double"/>
             </div>
             <div className="social-icons">
-                <div className="social-icon"><a href="https://discord.gg/ycyr8Fp3Wu" target="_blank" rel="noreferrer"><img src={Discord} alt='discord'/></a></div>
-                <div className="social-icon"><a href="https://twitter.com/double2winwin" target="_blank" rel="noreferrer"><img src={Twitter} alt='twitter'/></a></div>
-                <div className="social-icon"><a href="https://medium.com/@double2win/double-down-to-genesis-on-celo-be46547d22cf" target="_blank" rel="noreferrer"><img src={Medium} alt='medium'/></a></div>
-                <div className="social-icon"><a href="https://www.double2win.xyz/docs/" target="_blank" rel="noreferrer"><img src={Doc} alt='doc'/></a></div>
+                <div className="social-icon social-top"><a href="https://discord.gg/ycyr8Fp3Wu" target="_blank" rel="noreferrer"><img src={Discord} alt='discord'/></a></div>
+                <div className="social-icon social-top"><a href="https://twitter.com/double2winwin" target="_blank" rel="noreferrer"><img src={Twitter} alt='twitter'/></a></div>
+                <div className="social-icon social-top"><a href="https://medium.com/@double2win/double-down-to-genesis-on-celo-be46547d22cf" target="_blank" rel="noreferrer"><img src={Medium} alt='medium'/></a></div>
+                <div className="social-icon social-top"><a href="https://www.double2win.xyz/docs/" target="_blank" rel="noreferrer"><img src={Doc} alt='doc'/></a></div>
                 <a className={'primary-button'} href="https://app.double2win.xyz/">
                     <p>{'Launch App'}</p>
                 </a>
--- src/Page/Lend.js
+++ src/Page/Lend.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import MainImg from "../assets/main_Graphic.svg";
 import { ethers } from "ethers";
 import DescriiptorABI from "../abi/Descriptor.json";
+import Discord from '../assets/Discord Icon.svg'
+import Twitter from '../assets/Twitter Icon.svg'
+import Medium from '../assets/Medium Icon.svg'
+import Doc from '../assets/Doc Icon.svg'
 
 const provider = new ethers.providers.JsonRpcProvider(
   "https://goerli.infura.io/v3/a1ac3a707d90454eb92624852b96e497"
@@ -66,6 +70,12 @@ export default function Lend() {
         >
           <span>Double Down to Genesis on Celo</span>
         </a>
+      </div>
+      <div className="social-icons social-bottom">
+          <div className="social-icon"><a href="https://discord.gg/ycyr8Fp3Wu" target="_blank" rel="noreferrer"><img src={Discord} alt='discord'/></a></div>
+          <div className="social-icon"><a href="https://twitter.com/double2winwin" target="_blank" rel="noreferrer"><img src={Twitter} alt='twitter'/></a></div>
+          <div className="social-icon"><a href="https://medium.com/@double2win/double-down-to-genesis-on-celo-be46547d22cf" target="_blank" rel="noreferrer"><img src={Medium} alt='medium'/></a></div>
+          <div className="social-icon"><a href="https://www.double2win.xyz/docs/" target="_blank" rel="noreferrer"><img src={Doc} alt='doc'/></a></div>
       </div>
     </div>
   );
